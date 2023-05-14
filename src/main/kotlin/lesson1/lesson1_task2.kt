8d85c27d87d7c71ca22ac67889b64e741b5ace4f
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-14 21:50:30 +0300

    KS-1-2 работа с типами переменных и однострочными комментариями

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package lesson1
+
+fun main(){
+    val numberOfOrders : Int= 75
+    val templateForCustomers : String = "Благодарим за покупку!"
+    println(numberOfOrders)
+    println(templateForCustomers)
+
+    var numberOfEmployees : Int = 2000
+   // println(numberOfEmployees)
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}

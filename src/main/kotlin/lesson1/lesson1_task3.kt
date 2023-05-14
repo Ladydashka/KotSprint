adcf58ac3045157e890a0082060382262764e7b5
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-14 22:21:08 +0300

    KS-1-3 работа с типами переменных и конкатенацией строк

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,13 @@
+package lesson1
+fun main(){
+    val year = 1961
+    var hour = 9
+    var time = 7
+    println(year)
+    println(hour)
+    println(time)
+
+    hour = 10
+    time = 55
+    println("${hour}:${time}")
+}

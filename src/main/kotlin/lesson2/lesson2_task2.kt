30c74728564ef9f438ddbcbee76ad233911c70b8
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-15 18:19:29 +0300

    KS-2-2 арифметические операции

---
+++ src/main/kotlin/lesson2/lesson2_task2.kt
@@ -0,0 +1,10 @@
+package lesson2
+fun main(){
+    val constSalary = 50 * 30000
+    println(constSalary)
+    val totalConsumption = 30 * 20000 + constSalary
+    println(totalConsumption)
+    val averageSalary = totalConsumption / 80
+    println(averageSalary)
+}
+

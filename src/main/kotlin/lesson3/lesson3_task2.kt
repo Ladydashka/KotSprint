f49fa425c999ad915205b74482092b65706d8055
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-18 19:46:35 +0300

    KS-3-2 конкатенация строк

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package lesson3
+fun main(){
+    val firstName = "Татьяна"
+    val lastName = "Андреева"
+    val surname = "Сергеевна"
+    val age = 20
+    val newLastName = "Сидорова"
+    val newAge = 22
+    println("$lastName $firstName $surname,Возраст: $age")
+    println("$newLastName $firstName $surname,Возраст: $newAge")
+
+}
+

f39063c4bdd0e88f9d3beb050c853b57d2f95e0a
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-18 16:49:20 +0300

    KS-2-4 нахождение процентов от заданных значений

---
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -0,0 +1,10 @@
+package lesson2
+fun main() {
+    val ironOre = 11
+    val crystalOre = 7
+
+    val ironOreBuff = ironOre * 20 / 100
+    println(ironOreBuff)
+    val crystalOreBuff = crystalOre * 20 / 100
+    println(crystalOreBuff)
+}

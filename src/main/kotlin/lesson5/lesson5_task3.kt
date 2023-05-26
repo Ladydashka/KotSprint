64b1fea07615208c9671f1659dbb788adde3f394
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-26 19:25:13 +0300

    KS-5-3  алгоритм простого приложения-лотереи

---
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -0,0 +1,15 @@
+package lesson5
+
+fun main(){
+    println("Для участия в лотерее введите 2 числа от 1 до 100")
+    val number1 = readLine()?.toInt()
+    val number2 = readLine()?.toInt()
+
+   when{
+       number1 == FIRST_NUMBER && number2 == SECOND_NUMBER -> println("Поздравляем! Вы выиграли главный приз!")
+       number1 == FIRST_NUMBER || number2 == SECOND_NUMBER -> println("Вы выиграли утешительный приз!")
+       else -> println("Неудача! Крутите барабан!\nНомера победители 21 и 49")
+   }
+}
+const val FIRST_NUMBER = 21
+const val SECOND_NUMBER = 49

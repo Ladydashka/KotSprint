6b35a67355cf18b0eea7642bc710ed07829d1f6d
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-27 15:31:43 +0300

    KS-1-2 поправила пробелы в обозначении типа

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -1,12 +1,12 @@
 package lesson1
 
 fun main(){
-    val numberOfOrders : Int= 75
-    val templateForCustomers : String = "Благодарим за покупку!"
+    val numberOfOrders: Int = 75
+    val templateForCustomers: String = "Благодарим за покупку!"
     println(numberOfOrders)
     println(templateForCustomers)
 
-    var numberOfEmployees : Int = 2000
+    var numberOfEmployees: Int = 2000
    // println(numberOfEmployees)
     numberOfEmployees = 1999
     println(numberOfEmployees)

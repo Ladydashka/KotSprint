1a4a0d24c3d456c553617a1b838c25a306816e94
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-27 15:38:46 +0300

    KS-1-3 изменила на строковый тип

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -1,13 +1,13 @@
 package lesson1
 fun main(){
     val year = 1961
-    var hour = 9
-    var time = 7
+    var hour = "09"
+    var time = "07"
     println(year)
     println(hour)
     println(time)
 
-    hour = 10
-    time = 55
+    hour = "10"
+    time = "55"
     println("${hour}:${time}")
 }

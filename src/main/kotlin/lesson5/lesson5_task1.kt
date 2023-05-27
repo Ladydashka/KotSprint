d6f94b34758c2c3f1772973b9605ea5c0421c145
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-27 14:46:10 +0300

    KS-5-1 поменяла на оператор безопасного вызова, вынесла в отдельные переменные

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,8 +1,10 @@
 package lesson5
 
 fun main(){
-    println("Для входа в приложение необходимо сложить 2 + 2 и ввесли ответ")
-    val number = readLine()!!.toInt()
-    if (number == 4) println("Добро пожаловать!")
+    val example = "2 + 2"
+    val correctAnswer = 4
+    println("Для входа в приложение необходимо сложить $example и ввесли ответ")
+    val number = readln()?.toInt()
+    if (number == correctAnswer) println("Добро пожаловать!")
     else println("Доступ запрещен")
 }

fb6ad82036e661c05eea0c3c25604ba5bdebea7b
Ladydashka <lady.dasha.programming@gmail.com>
2023-05-27 16:07:15 +0300

    KS-1-5 привела значения к стринг и вывела значение на консоль

---
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -0,0 +1,30 @@
+package lesson1
+fun main(){
+    val seconds = 6480
+    val minutes =(seconds / 60).toString()
+    val secondInSpace = (seconds % 60).toString() + "0"
+    println("$minutes:$secondInSpace")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

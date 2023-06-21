8008ad3404dff30c3703aef6e7a495e653d1e324
Ladydashka <lady.dasha.programming@gmail.com>
2023-06-21 21:36:01 +0300

    KS-3-4 вывести вконсоль переменные ходов

---
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package lesson3
+
+fun main(){
+    var to = "E4"
+    var from = "E2"
+    var moveNumber = 1
+    val info = "[$from,$to;$moveNumber]"
+    println(info)
+
+    from = "D2"
+    to = "D3"
+    moveNumber = 2
+    println("[$from,$to;$moveNumber]")
+}
+

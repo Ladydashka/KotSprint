ab1f3b6b8302f8ee1358341c50965b2969e988d3
Ladydashka <lady.dasha.programming@gmail.com>
2023-06-22 22:40:23 +0300

    KS-5-1 убрала элвис из 7 строки

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -4,7 +4,7 @@ fun main(){
     val example = "2 + 2"
     val correctAnswer = 4
     println("Для входа в приложение необходимо сложить $example и ввесли ответ")
-    val number = readln()?.toInt()
+    val number = readln().toInt()
     if (number == correctAnswer) println("Добро пожаловать!")
     else println("Доступ запрещен")
 }

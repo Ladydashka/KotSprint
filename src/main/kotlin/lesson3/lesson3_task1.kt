d0b985c48b431c5ca32f3ca0e03721acc4cf6d93
Ladydashka <lady.dasha.programming@gmail.com>
2023-06-22 21:39:55 +0300

    KS-3-1 перезаписать переменные и вывести в консоль

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package lesson3
+
+fun main(){
+    var greeting = "Добрый день"
+    val name = "Дарья"
+    println("$greeting,$name")
+
+    greeting = "Добрый вечер"
+    println("$greeting,$name")
+}

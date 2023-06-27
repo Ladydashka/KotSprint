877092926a2af8fe1f9a528e92af8260db792b54
Ladydashka <lady.dasha.programming@gmail.com>
2023-06-27 18:52:26 +0300

    KS-10-1 небольшая игра кости

---
+++ src/main/kotlin/lessons_1_10/lesson4/lesson10/lesson10_task1.kt
@@ -0,0 +1,30 @@
+package lessons_1_10
+
+ fun main() {
+  val player = getValue()
+  val pc = getValue()
+
+  println("Игрок бросил кости. Значение кубиков $player")
+  println("Компьютер бросил кости. Значение кубиков $pc")
+
+  when {
+   player > pc -> println("Победило человечество.")
+   pc > player -> println("Победила машина.")
+   player == pc -> println("Необходимо бросить кубики еще раз")
+  }
+ }
+
+  fun getValue(): Int {
+   val value1 = (1..6).random()
+   val value2 = (1..6).random()
+   return value1 + value2
+  }
+
+
+
+
+
+
+
+
+
